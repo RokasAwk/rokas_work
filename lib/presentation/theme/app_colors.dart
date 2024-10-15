a17081305298e94d65797bc4efc1283ff6d5a5b4
Rokas <icestar6215@gmail.com>
2024-10-15 15:23:05 +0800

    style: update app colors and add app text style config

--- lib/presentation/theme/app_colors.dart
+++ lib/presentation/theme/app_colors.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AppColors {
   static const Color primaryColor = Color.fromARGB(255, 37, 37, 41);
-  static const Color black = Colors.black;
-  static Color black_50 = Colors.black.withOpacity(0.5);
-  static const Color white = Color.fromRGBO(255, 255, 255, 1);
-  static const Color yellow = Color.fromARGB(255, 255, 212, 82);
-  static Color blueGrey_10 = AppColors.primaryColor.withOpacity(0.1);
 
-  static const Color tileTitleColor = Color.fromARGB(255, 201, 201, 208);
-  static const Color tileBodyColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color blurGray = Colors.blueGrey;
+  static Color blueGrey_10 = AppColors.blurGray.withOpacity(0.1);
 
-  static const Color filterColor = Color.fromARGB(255, 240, 185, 163);
+  static const Color black = Color(0xFF000000);
+  static Color black_50 = AppColors.black.withOpacity(0.5);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color yellow = Color(0xFFF4CD7B);
+
+  static const Color neutral_10 = Color(0xFFFBFBFD);
+  static const Color neutral_20 = Color(0xFFF9F9FB);
+  static const Color neutral_40 = Color(0xFFF3F4F7);
+  static const Color neutral_50 = Color(0xFFEEEEF2);
+  static const Color neutral_80 = Color(0xFFE2E3EA);
+  static const Color neutral_100 = Color(0xFFC8C9D0);
+  static const Color neutral_200 = Color(0xFFA5A7B1);
+  static const Color neutral_300 = Color(0xFF888C9D);
+  static const Color neutral_400 = Color(0xFFB8B8B8);
+  static const Color neutral_500 = Color(0xFFA1A1A6);
+  static const Color neutral_600 = Color(0xFF62636A);
+  static const Color neutral_650 = Color(0xFF47484D);
+  static const Color neutral_700 = Color(0xFF333333);
+  static const Color neutral_800 = Color(0xFF252525);
+  static const Color neutral_900 = Color(0xFF19191B);
+
+  static const Color r_10 = Color(0xFFFDEDED);
+  static const Color r_20 = Color(0x32EB4747);
+  static const Color r_50 = Color(0xFFFDE9E9);
+  static const Color r_200 = Color(0xFFF5A3A3);
+  static const Color r_400 = Color(0xFFEB4747);
+  static const Color r_600 = Color(0xFF7B1F1F);
+  static const Color r_700 = Color(0xFF4B1919);
+
+  static const Color g_10 = Color(0xFFE7F8F2);
+  static const Color g_20 = Color(0x3214B881);
+  static const Color g_50 = Color(0xFFA6F2D3);
+  static const Color g_100 = Color(0xFF7AEBBD);
+  static const Color g_200 = Color(0xFF2BE79A);
+  static const Color g_300 = Color(0xFF25D88F);
+  static const Color g_400 = Color(0xFF1EC782);
+  static const Color g_500 = Color(0xFF14B881);
+  static const Color g_600 = Color(0xFF18A16A);
+  static const Color g_700 = Color(0xFF1A8A5C);
+
+  static const Color b_1 = Color(0xFF009EFD);
+  static const Color b_2 = Color(0xFF00C6FB);
+  static const Color b_3 = Color(0xFF005BEA);
+
+  static const List<Color> gradientColors = [b_1, b_2, b_3];
 }
---
+++ lib/presentation/theme/app_text_styles.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:rokas_work/presentation/theme/app_colors.dart';
+
+class AppTextStyles {
+  static const appW400Primary = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400PrimaryMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+    decoration: TextDecoration.none,
+    height: 11 / 7,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400PrimarySmall = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600Primary = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryColor,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600PrimaryMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryColor,
+    decoration: TextDecoration.none,
+    height: 11 / 7,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600PrimarySmall = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryColor,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400White = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400WhiteMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+    decoration: TextDecoration.none,
+    height: 11 / 7,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400WhiteSmall = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600White = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600WhiteMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    decoration: TextDecoration.none,
+    height: 11 / 7,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600WhiteSmall = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400N300 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.neutral_300,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400N300Medium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.neutral_300,
+    decoration: TextDecoration.none,
+    height: 11 / 7,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW400N300Small = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.neutral_300,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600N300 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.neutral_300,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600N300Medium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.neutral_300,
+    decoration: TextDecoration.none,
+    height: 11 / 7,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const appW600N300Small = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: AppColors.neutral_300,
+    decoration: TextDecoration.none,
+    height: 3 / 2,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+}
