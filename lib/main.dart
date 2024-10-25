4bdd7e29b5cfb85df2b87eb862b345967bf08f43
Rokas <icestar6215@gmail.com>
2024-10-25 13:39:03 +0800

    feat: add firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rokas_work/presentation/services/firebase_service/firbase_service.dart';
 
 import 'rokas_work_app.dart';
 
-void main() {
+bool shouldUseFirestoreEmulator = true;
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isIOS) {
+    await FirbaseService().initializeDefault();
+  }
+  if (Platform.isAndroid) {
+    await FirbaseService().initializeDefaultFromAndroidResource();
+  }
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
+  if (shouldUseFirestoreEmulator) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
+
   runApp(const ProviderScope(child: RokasWorkApp()));
 }
