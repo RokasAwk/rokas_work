6adfbd03105bc5916cdb13f9125f97eb304f3a9a
Rokas <icestar6215@gmail.com>
2024-10-28 23:01:53 +0800

    feat: memo list notifier add deleted, edit, update memo and onChangeMemoFilterType function

--- lib/presentation/pages/memo/memo_list_notifier.dart
+++ lib/presentation/pages/memo/memo_list_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rokas_work/domain/entity/memo.dart';
+import 'package:rokas_work/domain/value_object/memo_filter_type.dart';
 import 'package:rokas_work/presentation/services/firestore_service/firestore_memo_service.dart';
 
 import '../../routers/router.dart';
@@ -8,11 +9,29 @@ import 'memo_list_state.dart';
 abstract class MemoListNotifier extends StateNotifier<MemoListState> {
   MemoListNotifier(super.state);
 
-  void goToAddMemoPage();
+  void goToAddMemoPage(Memo? existedMemo);
 
   void addMemo({
     required String title,
     required String body,
+  });
+
+  void deleteMemo({
+    required int memoId,
+  });
+
+  void editMemo({
+    required Memo originMemoData,
+    required String title,
+    required String body,
+  });
+
+  void updateMemoList({
+    required List memoList,
+  });
+
+  void onChangeMemoFilterType({
+    required MemoFilterType type,
   });
 }
 
@@ -24,8 +43,8 @@ class MemoListNotifierImpl extends MemoListNotifier {
   }) : super(MemoListState.init());
 
   @override
-  void goToAddMemoPage() {
-    appRouter.push(const AddMemoRoute());
+  void goToAddMemoPage(Memo? existedMemo) {
+    appRouter.push(AddMemoRoute(existedMemo: existedMemo));
   }
 
   @override
@@ -41,5 +60,56 @@ class MemoListNotifierImpl extends MemoListNotifier {
       editedTime: DateTime.now(),
     );
     FirestoreMemoService().addData(memoData);
+  }
+
+  @override
+  void deleteMemo({
+    required int memoId,
+  }) {
+    FirestoreMemoService().deleteData("memo_$memoId");
+  }
+
+  @override
+  void editMemo({
+    required Memo originMemoData,
+    required String title,
+    required String body,
+  }) {
+    Memo updateMemo = Memo(
+        id: originMemoData.id,
+        title: title,
+        body: body,
+        createdTime: originMemoData.createdTime,
+        editedTime: DateTime.now(),
+        isPin: originMemoData.isPin,
+        isDeleted: false);
+    try {
+      FirestoreMemoService().upateData(
+        dataId: '${originMemoData.id}}',
+        data: updateMemo,
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  void updateMemoList({required List memoList}) {
+    state = state.copyWith(memoList: memoList as List<Memo>);
+  }
+
+  @override
+  void onChangeMemoFilterType({required MemoFilterType type}) {
+    List<Memo> sortedMemoList = state.memoList;
+    if (type == MemoFilterType.createdTime) {
+      sortedMemoList.sort((a, b) => a.createdTime.compareTo(b.createdTime));
+    } else {
+      sortedMemoList.sort((a, b) => a.editedTime.compareTo(b.editedTime));
+    }
+
+    state = state.copyWith(
+      filterType: type,
+      memoList: sortedMemoList,
+    );
   }
 }
