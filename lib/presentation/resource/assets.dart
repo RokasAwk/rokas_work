0e84526446bdf8feff62bd8d272c9ee3097c389e
Rokas <icestar6215@gmail.com>
2024-10-30 10:43:15 +0800

    chore: assets file name typo

--- lib/presentation/resource/assets.dart
+++ lib/presentation/resource/assets.dart
@@ -1,8 +1,9 @@
-class PngAssets {
+class Assets {
   static String punch1 = 'assets/punch1.png';
   static String punch2 = 'assets/punch2.png';
   static String punch3 = 'assets/punch3.png';
   static String punch4 = 'assets/punch4.png';
   static String punch5 = 'assets/punch5.png';
   static String punch6 = 'assets/punch6.png';
+  static String warrior = 'assets/warrior.jpg';
 }
