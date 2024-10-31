285f6ce3c867549dcb322480be04da2d688f90b2
Rokas <icestar6215@gmail.com>
2024-10-31 14:22:36 +0800

    feat: DevelopedByWidget add text style parameter

--- lib/presentation/pages/widgets/developed_by_widget.dart
+++ lib/presentation/pages/widgets/developed_by_widget.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:rokas_work/l10n/l10n.dart';
+import 'package:rokas_work/presentation/theme/app_text_styles.dart';
 
 class DevelopedByWidget extends StatelessWidget {
   const DevelopedByWidget({
     super.key,
+    this.textStyle,
   });
+
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
-    return Text(L10n.tr.common_develope_by);
+    return Text(
+      L10n.tr.common_develope_by,
+      style: textStyle ?? AppTextStyles.appW600PrimaryMedium,
+    );
   }
 }
