df42eb43b52a5f11e0cd054e5edc8724dd126cf3
Rokas <icestar6215@gmail.com>
2024-11-14 13:36:24 +0800

    style: change error state widget text and icon's color

--- lib/presentation/pages/widgets/error_state_widget .dart
+++ lib/presentation/pages/widgets/error_state_widget .dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:rokas_work/presentation/theme/app_text_styles.dart';
 
 import '../../theme/app_colors.dart';
 
 class ErrorStateWidget extends StatelessWidget {
-  const ErrorStateWidget({super.key});
+  const ErrorStateWidget({
+    super.key,
+    required this.onRetry,
+  });
+
+  final Function() onRetry;
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +21,27 @@ class ErrorStateWidget extends StatelessWidget {
         children: [
           const Icon(
             Icons.error,
-            color: AppColors.white,
+            color: AppColors.primaryColor,
           ),
           const SizedBox(height: 16),
           Text(
             errorMsg,
             style: const TextStyle(
               fontSize: 20,
-              color: AppColors.white,
+              color: AppColors.primaryColor,
             ),
           ),
+          TextButton.icon(
+            onPressed: onRetry,
+            icon: const Icon(
+              Icons.refresh,
+              color: AppColors.blueGray,
+            ),
+            label: const Text(
+              '重新嘗試',
+              style: AppTextStyles.appW600BlueGray,
+            ),
+          )
         ],
       ),
     );
