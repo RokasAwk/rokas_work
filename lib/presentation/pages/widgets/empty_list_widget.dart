0ee92eea7445cfbd1106328732aae876406d08d3
Rokas <icestar6215@gmail.com>
2025-02-17 12:25:42 +0800

    feat: EmptyListWidget add showPic

--- lib/presentation/pages/widgets/empty_list_widget.dart
+++ lib/presentation/pages/widgets/empty_list_widget.dart
@@ -1,34 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:rokas_work/presentation/resource/assets.dart';
 
 import '../../../l10n/l10n.dart';
 import '../../theme/app_colors.dart';
 
 class EmptyListWidget extends StatelessWidget {
-  const EmptyListWidget({super.key});
+  const EmptyListWidget({
+    super.key,
+    this.isShowPic = true,
+  });
+
+  final bool isShowPic;
 
   @override
   Widget build(BuildContext context) {
     String emptyMsg = L10n.tr.common_empty_item;
 
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(
-            Icons.error,
-            color: AppColors.black,
-            size: 36,
-          ),
-          const SizedBox(height: 16),
-          Text(
-            emptyMsg,
-            style: const TextStyle(
-              fontSize: 20,
-              color: AppColors.black,
-            ),
-          ),
-        ],
-      ),
+      child: Container(
+          color: AppColors.grey,
+          child: Column(
+            mainAxisAlignment:
+                isShowPic ? MainAxisAlignment.end : MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.error,
+                color: AppColors.black,
+                size: 36,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                emptyMsg,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: AppColors.black,
+                ),
+              ),
+              if (isShowPic) ...[Image.asset(Assets.holding)]
+            ],
+          )),
     );
   }
 }
